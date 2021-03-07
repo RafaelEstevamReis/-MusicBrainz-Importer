c757af1e869d3a0fa72dddfe99fcc33e18017cf9
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-07 19:14:38 -0300

    Update Program.cs

--- MB.Tests/Program.cs
+++ MB.Tests/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using MB.Lib;
 
@@ -12,22 +14,31 @@ namespace MB.Tests
         {
             Console.WriteLine("Hello World!");
 
-            FileReader fr = new FileReader(@"\mbdump\artist");
+            var extractor = new ModelExtractor(@"N:\MusicBrainz Database\mbdump\artist");
+            var model = extractor.BuildModel();
+
+
+            FileReader fr = new FileReader(@"N:\MusicBrainz Database\mbdump\artist");
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            int i = 0;
-            using var fs = File.OpenWrite("artistsSort.txt");
-            using var swr = new StreamWriter(fs, Encoding.UTF8);
-            foreach (var item in fr.Get<Lib.Models.Artist>())
-            {
-                i++;
-                swr.WriteLine($"{item.SortName}");
-            }
-            swr.Flush();
+            var names = fr.Get<Lib.Models.Artist>()
+                          .Select(a => a.Name);
+            var hashSet = new HashSet<string>(names);
+
+            File.WriteAllLines("artistsUnique.txt", hashSet.OrderBy(o => o));
+            //int i = 0;
+            //using var fs = File.OpenWrite("artists.txt");
+            //using var swr = new StreamWriter(fs, Encoding.UTF8);
+            //foreach (var item in fr.Get<Lib.Models.Artist>())
+            //{
+            //    i++;
+            //    swr.WriteLine($"{item.SortName}");
+            //}
+            //swr.Flush();
             sw.Stop();
-            Console.WriteLine($"End: Items: {i} Time: {sw.Elapsed}");
+            Console.WriteLine($"End: Items: {hashSet.Count} Time: {sw.Elapsed}");
 
         }
     }
