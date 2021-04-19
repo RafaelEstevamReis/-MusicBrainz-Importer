b06b406f26141c78754bafe7e9ba80c734766011
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-19 13:26:24 -0300

    Create model

---
+++ MB.Lib/Models/ArtistCredit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MB.Lib.Models
+{
+    public class ArtistCredit
+    {
+        // Sample: 202094
+        public int Id { get; set; }
+        // Sample: Ani & Beau
+        public string Name { get; set; }
+        // Sample: 1
+        public int C03 { get; set; }
+        // Sample: 6
+        public int C04 { get; set; }
+        // Sample: 2011-05-16 16:32:11.963929+00
+        public DateTime Created { get; set; }
+        // Sample: 0
+        public int C06 { get; set; }
+    }
+}
+
