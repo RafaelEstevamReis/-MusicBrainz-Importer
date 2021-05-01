24353384b66bc5994a31cb0818d8c4dc12a1c5a3
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-01 00:20:00 -0300

    Update libs

--- MB.Tests/Program.cs
+++ MB.Tests/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using MB.Lib;
+using Simple.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using MB.Lib;
 
 namespace MB.Tests
 {
@@ -17,18 +17,32 @@ namespace MB.Tests
             //var extractor = new ModelExtractor(@"N:\MusicBrainz Database\mbdump\artist");
             //var model = extractor.BuildModel();
 
-            string path = @"N:\MusicBrainz Database\mbdump\artist\";
+            string path = @"\\nas\home\Organizando\Dados Crawlers\MusicBrainz Database\mbdump\";
 
             FileReader fr = new FileReader($"{path}\\artist");
 
+            SqliteDB db = new SqliteDB("music.db");
+            db.CreateTables()
+              .Add<Artists>()
+              .Commit();
+
             Stopwatch sw = new Stopwatch();
             sw.Start();
+            int count = 0;
+            //var names = fr.Get<Lib.Models.Artist>()
+            //              .Select(a => a.Name);
+            //var hashSet = new HashSet<string>(names);
 
-            var names = fr.Get<Lib.Models.Artist>()
-                          .Select(a => a.Name);
-            var hashSet = new HashSet<string>(names);
+            var artists = fr.Get<Lib.Models.Artist>()
+                            .Select(o => new Artists()
+                            {
+                                Id = o.UID,
+                                Name = o.Name,
+                                SortName = o.SortName,
+                            });
+            db.BulkInsert(artists);
 
-            File.WriteAllLines("artistsUnique.txt", hashSet.OrderBy(o => o));
+            //File.WriteAllLines("artistsUnique.txt", hashSet.OrderBy(o => o));
             //int i = 0;
             //using var fs = File.OpenWrite("artists.txt");
             //using var swr = new StreamWriter(fs, Encoding.UTF8);
@@ -39,8 +53,14 @@ namespace MB.Tests
             //}
             //swr.Flush();
             sw.Stop();
-            Console.WriteLine($"End: Items: {hashSet.Count} Time: {sw.Elapsed}");
+            Console.WriteLine($"End: Items: {count} Time: {sw.Elapsed}");
 
         }
+    }
+    public class Artists
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SortName { get; set; }
     }
 }
